07272ee93f67b34efbdc2addf8a8973d09b88db0
최원재 <cwj980119@gmail.com>
2024-01-22 13:12:15 +0900

    Feat: Initial Commit [Backend]

---
+++ backend/src/test/java/com/ssafy/oywo/OywoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.oywo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OywoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
