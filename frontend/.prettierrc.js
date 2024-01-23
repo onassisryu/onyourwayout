662a8e6ecec9ab8abc2d0fbebfd7541d8ee853e6
이가영 <tp06100@naver.com>
2024-01-23 13:19:48 +0900

    S10P12A302-105 Feat:보일러 플레이트 생성

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 120,
+  endOfLine: 'auto',
+  quoteProps: 'consistent',
+};
