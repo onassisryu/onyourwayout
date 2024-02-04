0563788ae11a4a71f0f46efdefd890c0dcc292a2
이가영 <tp06100@naver.com>
2024-02-05 00:41:44 +0900

    Feat: 알림설정 페이지 제작 완료(S10P12A302-310)

--- frontend/assets/icons/index.ts
+++ frontend/assets/icons/index.ts
@@ -2,5 +2,3 @@
 // export {default as Home2} from './home2.svg';
 // export {default as mainLogo} from './back.png';
 export {default as puppy} from './puppy.svg';
-// export {default as searchIcon} from '../icons/search.png';
-// export {default as notificationIcon} from '../icons/bell_black.png';
--- frontend/src/components/Noticepage/NoticeHeader.tsx
+++ frontend/src/components/Noticepage/NoticeHeader.tsx
@@ -2,9 +2,11 @@
 import React, {useState, useEffect} from 'react';
 import styled from '@emotion/native';
 import {NavigationProp} from '@react-navigation/native';
-import theme from '@/Theme';
 import {GlobalContainer, GlobalText} from '@/GlobalStyles';
-import {View, TouchableOpacity, ImageSourcePropType} from 'react-native';
+import {TouchableOpacity} from 'react-native';
+import theme from '@/Theme';
+import GoBack from '@/components/Signup/GoBack';
+import Feather from 'react-native-vector-icons/Feather';
 
 const HeaderContainer = styled(GlobalContainer)`
   flex-direction: row;
@@ -16,22 +18,10 @@ const HeaderContainer = styled(GlobalContainer)`
   padding-right: 15px;
 `;
 
-const BackImage = styled.Image`
-  width: 25px;
-  height: 25px;
-  resize-mode: contain;
-`;
-
-const SettingsImage = styled.Image`
-  width: 25px;
-  height: 25px;
-  resize-mode: contain;
-`;
-
 const NoticeTitle = styled(GlobalText)`
   font-weight: bold;
-  font-size: ${theme.fontSize.subtitle};
-  color: ${theme.color.black};
+  font-size: ${props => props.theme.fontSize.subtitle};
+  color: ${props => props.theme.color.black};
   padding-bottom: 5px;
 `;
 
@@ -39,18 +29,13 @@ interface Props {
   navigation: NavigationProp<any>;
 }
 
-const backImage: ImageSourcePropType = require('icons/back.png');
-const settingsImage: ImageSourcePropType = require('icons/settings.png');
-
 const NoticeHeader = ({navigation}: Props) => {
   return (
     <HeaderContainer>
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <BackImage source={backImage}></BackImage>
-      </TouchableOpacity>
+      <GoBack />
       <NoticeTitle> 알림 </NoticeTitle>
       <TouchableOpacity onPress={() => navigation.navigate('NoticeSettings')}>
-        <SettingsImage source={settingsImage}></SettingsImage>
+        <Feather name="settings" size={25} color={theme.color.black} />
       </TouchableOpacity>
     </HeaderContainer>
   );
