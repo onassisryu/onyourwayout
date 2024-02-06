d22fbf1fa3d16334a78ae263984abf6ea7600106
김하연 <khy3496@cau.ac.kr>
2024-02-06 16:54:24 +0900

    Feat: 중복 닉네임, 전화번호, 아이디 확인 API 구현

--- backend/src/main/java/com/ssafy/oywo/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/oywo/controller/MemberController.java
@@ -207,4 +207,34 @@ public class MemberController {
         return new ResponseEntity<>(memberSerivce.findHoByInviteCode(inviteCode),HttpStatus.OK);
     }
 
+    // 아이디 중복 여부 확인
+    @GetMapping("/dup")
+    public ResponseEntity<?> duplicateUserName(@RequestParam("id") String userName){
+        boolean isExist=memberSerivce.isExistUserName(userName);
+        if (isExist){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // 전화번호 중복 여부 확인
+    @GetMapping("/dup")
+    public ResponseEntity<?> duplicatePhoneNumber(@RequestParam("phone") String phoneNumber){
+        boolean isExist= memberSerivce.isExistPhoneNumber(phoneNumber);
+        if (isExist){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // 닉네임 중복 여부 확인
+    @GetMapping("/dup")
+    public ResponseEntity<?> duplicateNickName(@RequestParam("nickname") String nickName){
+        boolean isExist= memberSerivce.isExistNickName(nickName);
+        if(isExist){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
