d93b66dfc5c9192d9805c09015cbeefd02da7dad
김하연 <khy3496@cau.ac.kr>
2024-02-06 15:45:27 +0900

    Fix : 채팅 메시지 이미지 저장

--- backend/src/main/java/com/ssafy/oywo/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/oywo/config/SecurityConfig.java
@@ -44,6 +44,8 @@ public class SecurityConfig {
                 .requestMatchers("/members/verify/**").permitAll()
                 // apart에 대한 정보 요청은 모두 허가
                 .requestMatchers(HttpMethod.GET,"/apart/**").permitAll()
+                .requestMatchers("/pub/**").permitAll()
+                .requestMatchers("/sub/**").permitAll()
                 // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
                 .anyRequest().authenticated()
                 .and()
--- backend/src/main/java/com/ssafy/oywo/dto/ChatMessageDto.java
+++ backend/src/main/java/com/ssafy/oywo/dto/ChatMessageDto.java
@@ -6,27 +6,41 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.Timestamp;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+
 public class ChatMessageDto {
-    private Long chatRoomId;            // 채팅방 id
-    private Long sendId;                // 보낸 사람 id
-    private String msg;                 // 보낸 메시지
-    private String imgURL;              // 이미지 주소
 
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Request{
+        private Long chatRoomId;            // 채팅방 id
+        private Long sendId;                // 보낸 사람 id
+        private String msg;                 // 보낸 메시지
+        private String img;                 // 바이트코드로 이미지를 받음
+    }
 
-    public ChatMessage toEntity(ChatRoom chatRoom){
-        return ChatMessage.builder()
-                .chatRoom(chatRoom)
-                .imgUrl(imgURL)
-                .msg(msg)
-                .senderId(sendId)
-                .build();
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Response{
+        private Long chatRoomId;            // 채팅방 id
+        private Long sendId;                // 보낸 사람 id
+        private String msg;                 // 보낸 메시지
+        private String imgUrl;              // 이미지 주소
 
+        public ChatMessage toEntity(ChatRoom chatRoom){
+            return ChatMessage.builder()
+                    .chatRoom(chatRoom)
+                    .msg(msg)
+                    .imgUrl(imgUrl)
+                    .senderId(sendId)
+                    .build();
+        }
     }
 }
