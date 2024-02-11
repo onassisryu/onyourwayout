be7b765e1c69facc7d9d76362b596e42d3f8eb58
김하연 <khy3496@cau.ac.kr>
2024-02-12 08:23:18 +0900

    Fix: ChatController convertAndSend 경로 수정

--- backend/src/main/java/com/ssafy/oywo/controller/ChatController.java
+++ backend/src/main/java/com/ssafy/oywo/controller/ChatController.java
@@ -49,7 +49,8 @@ public class ChatController {
                 .imgUrl(imgUrl).build();
 
         // /room/{roomId}를 구독하는 클라이언트로 메시지 객체가 전달
-        sendingOperations.convertAndSend("/sub/channel"+response.getChatRoomId(),response);
+        sendingOperations.convertAndSend("/sub/channel/"+response.getChatRoomId(),response);
+        log.info("메시지 전송 성공");
 
         // message를 저장한다.
         chatService.saveChatMessage(response);
