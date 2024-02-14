a6f32d0d97d9baa6f1e2cf5db094d93702b5841d
김하연 <khy3496@cau.ac.kr>
2024-02-14 12:01:04 +0900

    Fix : Entity infinite recursion error 수정

--- backend/src/main/java/com/ssafy/oywo/repository/NotiDongRepository.java
+++ backend/src/main/java/com/ssafy/oywo/repository/NotiDongRepository.java
@@ -2,7 +2,13 @@ package com.ssafy.oywo.repository;
 
 import com.ssafy.oywo.entity.NotiDong;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface NotiDongRepository extends JpaRepository<NotiDong,Long> {
     void deleteAllByMemberId(Long memberId);
+
+    @Query("select notiDong from NotiDong notiDong where notiDong.member.id= :memberId and notiDong.dongId= :dongId")
+    Optional<NotiDong> findByMemberIdAndDongId(Long memberId, Long dongId);
 }
