eb86cdab9caed875b61ee99918ced538ac99f118
류지수 <onyourown793@gmail.com>
2024-02-15 10:08:04 +0900

    feat: 현재 내 거래 반환값에 ho 추가

--- backend/src/main/java/com/ssafy/oywo/service/DealService.java
+++ backend/src/main/java/com/ssafy/oywo/service/DealService.java
@@ -23,7 +23,7 @@ public interface DealService {
     // 요청자/수락자 현재 거래 조회
     List<DealDto.ResponseWithHo> getDealsBetweenUsers(Long requestId, Long acceptId);
     // 내가 나온김에 해야할 일
-    List<DealDto.Response> getMyDealsByStatusING();
+    List<DealDto.ResponseWithHo> getMyDealsByStatusING();
     // 거래 생성
     DealDto.Response createDeal(DealDto.Request dto, List<MultipartFile> dealImageFileList);
     // 거래 하나 조회
